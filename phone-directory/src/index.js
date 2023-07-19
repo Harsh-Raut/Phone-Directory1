096f6ed57c9c96456bcf580504aa594ed25a0195
Harsh-Raut <rautharsh.00@gmail.com>
2023-07-19 16:22:21 +0530

    Added routing for Add Subscriber page and Show Subscribers page

--- phone-directory/src/index.js
+++ phone-directory/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import AddSusbscriber from './AddSubscriber';
 import './common/common.css';
 import PhoneDirectory from './PhoneDirectory';
 
