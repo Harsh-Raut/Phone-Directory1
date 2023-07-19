dfa25775713cdbf8e610db49e2b6a02b064f3e08
Harsh-Raut <rautharsh.00@gmail.com>
2023-07-19 14:11:36 +0530

    Created Skeleton of Home Page

--- phone-directory/src/App.js
+++ phone-directory/src/App.js
@@ -1,11 +1,22 @@
+import React,{Component} from React;
 
-
-function App() {
-  return (
-    <div >
-      Phone Directory
+class App extends Component {
+  render(){
+    return (
+    <div  >
+      {/* <div className="header">Phone Directory</div>
+      
+      <button> Add</button>
+      <div>
+        <span>Name</span></br>
+      <span>Phone</span>
+      </div>  */}
+      <label htmlFor="name"></label>
+      <input id="name" type="text" placeholder="type here" defaultvalue="shristi"></input>
     </div>
   );
+  }
+  
 }
 
 export default App;
