ed9da8ed81ea557fbb4afed7e51240c495146b2f
Harsh-Raut <rautharsh.00@gmail.com>
2023-07-19 16:04:28 +0530

    Added state in ‘AddSubscriber’ and ‘App’ components

--- phone-directory/src/App.js
+++ phone-directory/src/App.js
@@ -2,19 +2,15 @@ import React,{Component} from "react";
 import Header from "./Header";
 import './App.css';
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      subscribersListToShow: []
+    }
+  }
+
   render(){
-    let subscribers = [
-      {
-        id: 1,
-        name: "Shilpa Bhat",
-        phone: "8888888888"
-      },
-      {
-        id: 2,
-        name: "Srishti Gupta",
-        phone: "9999999999"
-      }
-    ];
+    
     return (
     <div>
       
@@ -27,7 +23,7 @@ class App extends Component {
             <span className="grid-item phone-heading">Phone</span>
           </div>
           {
-            subscribers.map(sub => {
+             this.state.subscribersListToShow.map(sub => {
               return <div key={sub.id} className="grid-container">
                 <span className="grid-item">{sub.name}</span>
                 <span className="grid-item">{sub.phone}</span>
--- phone-directory/src/index.js
+++ phone-directory/src/index.js
@@ -5,5 +5,5 @@ import AddSusbscriber from './AddSubscriber';
 import './common/common.css';
 
 
-ReactDOM.render(<AddSusbscriber />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
