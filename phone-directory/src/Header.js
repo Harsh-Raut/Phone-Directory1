49e82ee40a6fe65bae7ae34a7f4a45ee23511140
Harsh-Raut <rautharsh.00@gmail.com>
2023-07-19 14:48:03 +0530

    Styled ‘Header’ (External Styling)

--- phone-directory/src/Header.js
+++ phone-directory/src/Header.js
@@ -1,13 +1,7 @@
 import React,{Component} from "react";
 
 const Header=function(){
-    const headerStyle = {
-        textAlign: 'center',
-        padding: 20,
-        background: '#000',
-        color: '#fff',
-        textTransform: 'uppercase'
-     };
+    
     return(
         <div className="header" style={headerStyle}>
             Phone Directory
